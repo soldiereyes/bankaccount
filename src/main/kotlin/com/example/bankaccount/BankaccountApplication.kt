61fc83c9e09d1acea0a57d74d2bf9da935fc514b
Guilherme Gonzaga <guilherme.gonzaga@eprecise.com.br>
2022-09-15 22:24:47 -0300

    Initial commit

---
+++ src/main/kotlin/com/example/bankaccount/BankaccountApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bankaccount
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankaccountApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankaccountApplication>(*args)
+}
